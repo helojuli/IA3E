1fded0990cf549a5255c718771380e3242de9eab
helojuli <146955981+helojuli@users.noreply.github.com>
2024-08-02 09:29:07 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,14 @@
+const caixapricipal = documnet.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaalternativa = documnet.querySelector('.caixa-alternativa');
+const caixaresultado = documnet.querySelector('.caixa-resultado');
+
+const pergunta = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"pergunta 1",
+        alternativas:[
+            "alternativas 1",
+            "alternativa 2"
+        ]
+    }
+]
